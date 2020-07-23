6b9801e6d9c2ece046b45794cc941c2e89cceecc
Israel Junior <israeljrs@gmail.com>
2020-07-23 12:04:12 -0300

    fix uuid to id keys

--- db/migrate/20200723143727_rolify_create_roles.rb
+++ db/migrate/20200723143727_rolify_create_roles.rb
@@ -1,17 +1,18 @@
 class RolifyCreateRoles < ActiveRecord::Migration[5.2]
   def change
-    create_table(:roles) do |t|
+    create_table(:roles, id: :uuid) do |t|
       t.string :name
+      t.uuid :resource_id
       t.references :resource, :polymorphic => true
 
       t.timestamps
     end
 
     create_table(:users_roles, :id => false) do |t|
-      t.references :user
-      t.references :role
+      t.uuid :user_id
+      t.uuid :role_id
     end
-    
+
     add_index(:roles, [ :name, :resource_type, :resource_id ])
     add_index(:users_roles, [ :user_id, :role_id ])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 2020_07_23_143729) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", force: :cascade do |t|
+  create_table "roles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
-    t.string "resource_type"
     t.bigint "resource_id"
+    t.string "resource_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
@@ -55,11 +55,9 @@ ActiveRecord::Schema.define(version: 2020_07_23_143729) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.uuid "user_id"
+    t.uuid "role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
 end
