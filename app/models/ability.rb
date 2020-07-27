e85eb705c3104a30bd2e679f2afdb6128b964a77
Israel Junior <israeljrs@gmail.com>
2020-07-27 09:42:38 -0300

    fix permission of rolify

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,9 @@ class Ability
   def initialize(user)
     if user.has_role? :admin
       can :manage, :all
+    elsif user.has_role? :user
+      can :manage, Movie
+      can :manage, MovieView
     else
       can :read, :all
     end
